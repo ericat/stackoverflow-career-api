36d132893e878c15b54f7dc4b54040e9e114ac1d
Ericat <erica.salvaneschi@hotmail.com>
2014-02-03 17:18:39 +0000

    Initial commit

---
+++ job-scraper.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
+
+url = 'http://careers.stackoverflow.com/jobs'
+
+page = Nokogiri::HTML(open(url))
+
+rows = page.css('.list.jobs div[data-jobid]')
+
+def posted
+  Time.now
+end
+
+hash = rows.map do |row|
+  { 
+    job_id: row['data-jobid'],
+    title: row.css('a.job-link').first.text,
+    url: row.css('a.job-link').first['href'],
+    jscore: row.css('.joeltestscore').text,
+    company: row.css('p.location').text.split(' - ')[0].strip!,
+    location: row.css('p.location').text.split(' - ')[1].strip!,
+    tags: [row.css('a.post-tag.job-link').map(&:text)].flatten,
+    # posted: posted
+  }
+
+end
+
+pp hash
+
+
+
