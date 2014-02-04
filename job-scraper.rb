89475cda9e572af716efa4dca106d742d747a934
Michael <michaelnicholasfisher@gmail.com>
2014-02-04 12:14:24 +0000

    Extracts jobscraper into JobScraper class

--- job-scraper.rb
+++ job-scraper.rb
@@ -3,35 +3,41 @@ require 'nokogiri'
 require 'open-uri'
 require 'pp'
 
-url = 'http://careers.stackoverflow.com/jobs'
-page = Nokogiri::HTML(open(url))
+class JobScraper
 
+  def initialize(url)
+    # url = 'http://careers.stackoverflow.com/jobs'
+    @page = Nokogiri::HTML(open(url))
+    @rows = @page.css('.list.jobs div[data-jobid]')
+    @last_page = @page.css('div.pagination a.job-link')[-2].text.to_i
+  end
 
-last_page = page.css('div.pagination a.job-link')[-2].text.to_i
+  def build_urls
+   # ["http://careers.stackoverflow.com/jobs?pg=1", "http://careers.stackoverflow.com/jobs?pg=2", "http://careers.stackoverflow.com/jobs?pg=3"]
+   urls = []
+   (1..@last_page).each {|n| urls << "http://careers.stackoverflow.com/jobs?pg=#{n}"} 
+    urls
+  end
 
-urls = ["http://careers.stackoverflow.com/jobs?pg=1", "http://careers.stackoverflow.com/jobs?pg=2", "http://careers.stackoverflow.com/jobs?pg=3"]
-
-# urls = []
-# (1..last_page).each {|n| urls << "http://careers.stackoverflow.com/jobs?pg=#{n}"} 
-
-
-rows = page.css('.list.jobs div[data-jobid]')
-
-hash = urls.map do |url| 
-
-  rows.map do |row| 
-    { job_id: row['data-jobid'],
-      title: row.css('a.job-link').first.text,
-      description: row.css('p.description').text,
-      url: row.css('a.job-link').first['href'],
-      jscore: row.css('.joeltestscore').text,
-      company: row.css('p.location').text.split(' - ')[0].strip!,
-      location: row.css('p.location').text.split(' - ')[1].strip!,
-      tags: [row.css('a.post-tag.job-link').map(&:text)].flatten,
-    # posted: posted
-    }
+  
+  def scrape 
+    @urls = build_urls 
+    @urls.map do |url| 
+      @rows.map do |row| 
+        { job_id: row['data-jobid'],
+          title: row.css('a.job-link').first.text,
+          description: row.css('p.description').text,
+          url: row.css('a.job-link').first['href'],
+          jscore: row.css('.joeltestscore').text,
+          company: row.css('p.location').text.split(' - ')[0].strip!,
+          location: row.css('p.location').text.split(' - ')[1].strip!,
+          tags: [row.css('a.post-tag.job-link').map(&:text)].flatten,
+        # posted: posted
+        }
+      end
+    end
   end
 
 end
 
-pp hash
+# pp JobScraper.new('http://careers.stackoverflow.com/jobs').scrape
