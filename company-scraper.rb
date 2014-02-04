7ac9f24c9aaedfafa1da9e59c1c4723293ca569b
koomerang <k.veluppillai@googlemail.com>
2014-02-04 11:38:16 +0000

    Added pagination scrape for company pages

--- company-scraper.rb
+++ company-scraper.rb
@@ -1,18 +1,40 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'pp'
 
-url = 'http://careers.stackoverflow.com/company/workshare'
+url = 'http://careers.stackoverflow.com/jobs/companies'
 
 page = Nokogiri::HTML(open(url))
 
-company = { name: page.css('h1').text,
-            avatar: page.css('div.logo-container img').first['src'],
-            size: page.css('table.basics tr').first.children[1].text,
-            status: page.css('table.basics tr')[1].children[1].text,
-            founded: page.css('table.basics tr')[2].children[1].text,
-            url: page.css('a.cp-links-url').text,
-            tags: page.css('div.tags span.post-tag').map(&:text),
-            benefits_list: page.css('div.benefits-list span.benefit').map(&:text),
-            jobs: page.css('div.job a').map {|link| link[:href][/\d+/] }
-          }
+last_page = page.css('div.pagination a.job-link')[-2].text.to_i
+
+urls = ["http://careers.stackoverflow.com/jobs/companies?pg=1", "http://careers.stackoverflow.com/jobs/companies?pg=2" ]
+
+# urls = []
+# (1..last_page).each {|n| urls << "http://careers.stackoverflow.com/jobs/companies?pg=#{n}"} 
+
+company_urls = []
+
+urls.map do |url|
+  page = Nokogiri::HTML(open(url))
+  company_urls << page.css('.list.companies a.title').map {|x| "http://careers.stackoverflow.com/uk" + x['href']}
+  company_urls.flatten!
+end
+
+companies = company_urls.map do |company_url|
+
+  page = Nokogiri::HTML(open(company_url))
+
+    { name: page.css('h1').text,
+      avatar: page.css('div.logo-container img').first['src'],
+      size: (page.css('table.basics tr').first.children[1].text rescue nil),
+      status: (page.css('table.basics tr')[1].children[1].text rescue nil),
+      founded: (page.css('table.basics tr')[2].children[1].text rescue nil),
+      url: page.css('a.cp-links-url').text,
+      id: company_url.split('/').last,
+      tags: page.css('div.tags span.post-tag').map(&:text),
+      benefits_list: page.css('div.benefits-list span.benefit').map(&:text),
+      jobs: page.css('div.job a').map {|link| link[:href][/\d+/] }
+    }
+end
--- job-scraper.rb
+++ job-scraper.rb
@@ -1,34 +1,37 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'pp'
 
 url = 'http://careers.stackoverflow.com/jobs'
 page = Nokogiri::HTML(open(url))
 
-last_page = page.css('div.pagination a.job-link')[-2].text.to_i
 
-# urls = []
-(1..last_page).each {|n| urls << "http://careers.stackoverflow.com/jobs?pg=#{n}"} 
+last_page = page.css('div.pagination a.job-link')[-2].text.to_i
 
 urls = ["http://careers.stackoverflow.com/jobs?pg=1", "http://careers.stackoverflow.com/jobs?pg=2", "http://careers.stackoverflow.com/jobs?pg=3"]
 
+# urls = []
+# (1..last_page).each {|n| urls << "http://careers.stackoverflow.com/jobs?pg=#{n}"} 
+
+
 rows = page.css('.list.jobs div[data-jobid]')
 
+hash = urls.map do |url| 
 
+  rows.map do |row| 
+    { job_id: row['data-jobid'],
+      title: row.css('a.job-link').first.text,
+      description: row.css('p.description').text,
+      url: row.css('a.job-link').first['href'],
+      jscore: row.css('.joeltestscore').text,
+      company: row.css('p.location').text.split(' - ')[0].strip!,
+      location: row.css('p.location').text.split(' - ')[1].strip!,
+      tags: [row.css('a.post-tag.job-link').map(&:text)].flatten,
+    # posted: posted
+    }
+  end
 
+end
 
-# rows.map do |row| 
-#   { job_id: row['data-jobid'],
-#     title: row.css('a.job-link').first.text,
-#     description: row.css('p.description').text,
-#     url: row.css('a.job-link').first['href'],
-#     jscore: row.css('.joeltestscore').text,
-#     company: row.css('p.location').text.split(' - ')[0].strip!,
-#     location: row.css('p.location').text.split(' - ')[1].strip!,
-#     tags: [row.css('a.post-tag.job-link').map(&:text)].flatten,
-#   # posted: posted
-#   }
-# end
-
-
-
+pp hash
