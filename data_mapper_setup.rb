d6e849a88d474e7ec26f24d5e2a78979c1083316
Ericat <erica.salvaneschi@hotmail.com>
2014-02-12 01:28:51 +0000

    Fixed Datamapper config

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/stackoverflow-api-#{env}")
+DataMapper.setup(:default, ENV["HEROKU_POSTGRESQL_SILVER_URL"] || "postgres://localhost/stackoverflow-api-#{env}")
 
 DataMapper.finalize
 
